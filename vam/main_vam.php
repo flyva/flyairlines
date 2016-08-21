e51f79b8b12c05bc93058c78d18dc7f4500a8b51
elodie-adactive <elodie.philippe@adactive.com>
2016-08-21 03:20:54 +0200

    Start pilot strip

--- vam/main_vam.php
+++ vam/main_vam.php
@@ -182,51 +182,7 @@ include('get_pilot_data.php');
     </div>
 </header>
 
-<div class="row">
-    <div class="col-md-12">
-        <div id="carousel">
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-                </ol>
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item active"><img src="./images/slider/1.jpg" alt="...">
-
-                        <div class="carousel-caption"><h3>Virtual Airlines Manager</h3></div>
-                    </div>
-                    <div class="item"><img src="./images/slider/2.jpg" alt="...">
-
-                        <div class="carousel-caption"><h3>Virtual Airlines Manager</h3></div>
-                    </div>
-                    <div class="item"><img src="./images/slider/3.jpg" alt="...">
-
-                        <div class="carousel-caption"><h3>Virtual Airlines Manager</h3></div>
-                    </div>
-                    <div class="item"><img src="./images/slider/4.jpg" alt="...">
-
-                        <div class="carousel-caption"><h3>Virtual Airlines Manager</h3></div>
-                    </div>
-                    <div class="item"><img src="./images/slider/5.jpg" alt="...">
-
-                        <div class="carousel-caption"><h3>Virtual Airlines Manager</h3></div>
-                    </div>
-                </div>
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span> </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button"
-                   data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span> </a></div>
-            <!-- Carousel -->
-        </div>
-    </div>
-</div>
-<div class="container">
+<div class="container bot">
     <?php include('pilot_profile_row1.php'); ?>
 
     <?php $medal_pilot = $id;
